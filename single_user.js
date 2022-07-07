e5426557080ff6abc5f89ec724178d99663f7ddf
Aarika <aarika.srivastava@gmail.com>
2022-07-08 00:06:01 +0530

    correction in report content

--- single_user.js
+++ single_user.js
@@ -1,8 +1,15 @@
 
 function displayContent () {
+  var arr_new= new Array()
+  arr_new[0]=["Customer Name","Transaction Number","Result"];
   annual_transaction = document.getElementById('totalAnnualTransactions').value;
   digital_enrollment = document.getElementById('digitalEnrollmentTransactions').value;
     result = TimeSavingCalculate(annual_transaction , digital_enrollment); 
+  arr_new[1]=["Aarika_Single"," ",result.toString()];
+  res=makeTable(arr_new);
+  html2pdf().from(res).toPdf().save(arr_new[1][0]);
+  
+
   console.log(result);
 
 
--- submit_button_fun.js
+++ submit_button_fun.js
@@ -2,22 +2,24 @@
   function myFunction(){
 
     readXlsxFile(excel_file.files[0]).then (function(data){
-          //console.log(data);
-          data[0].push("Result");
-          
-          for  (i= 1 ; i<data.length ; i++){
-            
-            //calling calculate function
-            result = TimeSavingCalculate(data[i][2],data[i][3]);
-             data[i].push(result); // pushing the result into report
-         
-            var arr= new Array()
-            arr[0]=data[0]
-            arr[1]=data[i]
-            tbl=makeTable(arr);
-            // html to pdf download
-            html2pdf().from(tbl).toPdf().save(arr[1][0]);
-       console.log(result);
+      //console.log(data);
+      data[0].push("Result");
+data[0].splice(2,2);
+      
+      for  (i= 1 ; i<data.length ; i++){
+        
+        //calling calculate function
+        result = TimeSavingCalculate(data[i][2],data[i][3]);
+         data[i].push(result); // pushing the result into report
+        
+  data[i].splice(2,2);
+        var arr= new Array()
+        arr[0]=data[0]
+        arr[1]=data[i]
+        tbl=makeTable(arr);
+        // html to pdf download
+        html2pdf().from(tbl).toPdf().save(arr[1][0]);
+   console.log(result);
      
   
             
