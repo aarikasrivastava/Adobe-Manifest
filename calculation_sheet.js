411c14d4e946649a577b6d962ec73b315337fd30
Aarika <aarika.srivastava@gmail.com>
2022-07-22 13:06:22 +0530

    fixed the decimal problem

--- calculation_sheet.js
+++ calculation_sheet.js
@@ -1,20 +1,20 @@
 function TimeSavingCalculate(Number_of_annual_transactions){
   if (USD){}
 TimeSaving_hrs = (parseFloat(Number_of_annual_transactions)*parseFloat(timeSavedTransactions)*0.25).toFixed(2);
-enrollmentSaving_hrs = (totalDigitalEnrollment* timeSavedEnrollment *0.50).toFixed(2) ;
+enrollmentSaving_hrs = (totalDigitalEnrollment* timeSavedEnrollment *0.50).toFixed() ;
 return TimeSaving_hrs + "/" + enrollmentSaving_hrs;
 }
 
 function increasedTransactions(Number_of_annual_transactions) {
-  number_increased_transaction = ((parseFloat(Number_of_annual_transactions) + parseFloat(totalDigitalEnrollment)) * 0.50).toFixed(2);
+  number_increased_transaction = ((parseFloat(Number_of_annual_transactions) + parseFloat(totalDigitalEnrollment)) * 0.50).toFixed();
 
   return number_increased_transaction;
 }
 
 function TimeEnrollment$(){
  
- TimeSaving_$=  (0.80 * (TimeSaving_hrs * averageCompensationTransactions * 0.50)).toFixed(2);
- EnrollmentSaving_$ = ((enrollmentSaving_hrs * averageCompensationEnrollments * 0.50)  * 0.80).toFixed(2) ;
+ TimeSaving_$=  (0.80 * (TimeSaving_hrs * averageCompensationTransactions * 0.50)).toFixed();
+ EnrollmentSaving_$ = ((enrollmentSaving_hrs * averageCompensationEnrollments * 0.50)  * 0.80).toFixed() ;
  if (USD)
 return TimeSaving_$   + " / " + EnrollmentSaving_$+" USD"; 
 if (EURO) {
@@ -35,12 +35,12 @@ function customerExperienceValue(Number_of_annual_transactions) {
   a= (reducedDropOffRate *0.01) * parseFloat(totalDigitalEnrollment) * avgRevenuePerTrans *0.10 ;
   b= number_increased_transaction *250*0.10 *0.10; 
   c= (parseFloat(Number_of_annual_transactions) * 0.025 * 0.50 * 0.50)* averageCompensationEnrollments*0.50;
-  customer_value=((parseFloat(a) + parseFloat(b)+parseFloat(c)) *0.85) .toFixed(2);
+  customer_value=((parseFloat(a) + parseFloat(b)+parseFloat(c)) *0.85) .toFixed();
   return  customer_value ;
 }
 
 function complianceEfficiency(){
-  compliance = parseFloat(((employeesComplianceRegulatory*hoursSpentEmployeeComplianceRegulatory * 52* percentReductionComplianceRegulatory*0.01) * compensationComplianceRegulatoryStaff *0.85)).toFixed(2);
+  compliance = parseFloat(((employeesComplianceRegulatory*hoursSpentEmployeeComplianceRegulatory * 52* percentReductionComplianceRegulatory*0.01) * compensationComplianceRegulatoryStaff *0.85)).toFixed();
  return compliance; 
 
 }
@@ -48,12 +48,12 @@ function complianceEfficiency(){
 function legacyEfficiency(Number_of_annual_transactions) {
   f1 = (parseFloat(Number_of_annual_transactions) + parseFloat(totalDigitalEnrollment) +parseFloat((totalDigitalEnrollment*reducedDropOffRate*0.01)) +parseFloat( number_increased_transaction ))*1.2;
   legalEff =  (parseFloat(f1)+ parseFloat((f1*percentCostESign*0.01)) ) * 0.95;
-  return parseFloat(legalEff).toFixed(2);
+  return parseFloat(legalEff).toFixed();
 }
 function sustainbilitySaving(Number_of_annual_transactions){
   a1=(parseFloat(Number_of_annual_transactions)+ parseFloat(totalDigitalEnrollment)+ parseFloat(reducedDropOffRate*0.01 *totalDigitalEnrollment) + parseFloat(number_increased_transaction))*0.80;
   b1= a1 *0.50 ; 
-  sustainbility = ((parseFloat(a1 * printingScanningFaxingSaving )+ parseFloat(b1*mailingScanningFaxingSaving))*0.85).toFixed(2);
+  sustainbility = ((parseFloat(a1 * printingScanningFaxingSaving )+ parseFloat(b1*mailingScanningFaxingSaving))*0.85).toFixed();
   
   if (USD) return sustainbility + "USD" ; 
   if(EURO) return sustainbility +"EURO";
