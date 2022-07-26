a154f84b33cb482866f1be633f32a0d628d61237
Aarika <aarika.srivastava@gmail.com>
2022-07-26 20:32:34 +0530

    removing transaction number

--- submit_button_fun.js
+++ submit_button_fun.js
@@ -11,19 +11,19 @@
             data[0].push("Legal Efficiency");
             data[0].push("Sustainbility Savings");
             data[0].push("Total Savings");
-      data[0].splice(2,1);
+      //data[0].splice(2,1);
             
             for  (i= 1 ; i<data.length ; i++){
               
                 Number_of_digital_enrollments = 0.25*data[i][2];
               //calling calculate function
-              result = TimeSavingCalculate(data[i][2]);
-              increase_transactions= increasedTransactions(data[i][2]);
+              result = TimeSavingCalculate(data[i][1]);
+              increase_transactions= increasedTransactions(data[i][1]);
               time_enrollment_$ = TimeEnrollment$();
-              customerExperience = customerExperienceValue(data[i][2]);
+              customerExperience = customerExperienceValue(data[i][1]);
               compliance_efficiency= complianceEfficiency();
-              legal_efficiency = legacyEfficiency(data[i][2]);
-              sustainbility_savings = sustainbilitySaving(data[i][2]);
+              legal_efficiency = legacyEfficiency(data[i][1]);
+              sustainbility_savings = sustainbilitySaving(data[i][1]);
               total_saving = TotalSavings();
                data[i].push(result); // pushing the result into report
                data[i].push(increase_transactions);
@@ -33,7 +33,7 @@
                data[i].push(legal_efficiency);
                data[i].push(sustainbility_savings);
                data[i].push(total_saving);
-        data[i].splice(2,1);
+        //data[i].splice(2,1);
               var arr= new Array()
               arr[0]=data[0]
               arr[1]=data[i]
